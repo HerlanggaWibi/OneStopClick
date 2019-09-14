9aaad88464d201c02c6515125cdb50f156c070de
herlangga wibi <herlanggawibi@herlanggas-macbook-pro.local>
2019-09-14 17:45:05 +0700

    Initial Commit

---
+++ OneStopClick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneStopClick
+//
+//  Created by herlangga wibi on 14/09/19.
+//  Copyright © 2019 herlangga wibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
