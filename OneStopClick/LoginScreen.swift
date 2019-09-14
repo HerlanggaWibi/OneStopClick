7b967fed0de8c2e13b59797805ed34271c96f37e
herlangga wibi <herlanggawibi@herlanggas-mbp>
2019-09-14 20:37:49 +0700

    change logo with .png

---
+++ OneStopClick/LoginScreen.swift
@@ -0,0 +1,63 @@
+//
+//  LoginScreen.swift
+//  OneStopClick
+//
+//  Created by herlangga wibi on 14/09/19.
+//  Copyright © 2019 herlangga wibi. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var passWord: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        userName.beautifulTextField()
+        passWord.beautifulTextField()
+        signInButton.beautifulButton()
+    }
+    
+
+}
+
+extension UITextField {
+    func beautifulTextField() {
+        self.borderStyle = .none
+        self.layer.masksToBounds = false
+        //To apply corner radius
+        self.layer.cornerRadius = self.frame.size.height / 3
+        
+        //To apply border radius
+        self.layer.borderWidth = 0.25
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        
+        //To apply shadow
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 4
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0)
+        
+    }
+}
+
+extension UIButton {
+    func beautifulButton() {
+        
+        //Adding corner radius
+        self.layer.cornerRadius = 7
+        
+        //Adding shadow 
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 4
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
+}
